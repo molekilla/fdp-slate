aa13e6d9f1e2076d27cb253cd745ad6c110ce1a9
Miguel Lopez Detrinidad <mlopezdetrinidad@Miguels-MacBook-Pro.local>
2023-04-11 06:57:59 -0600

    added logic for richtext toolbar

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { createEditor } from 'slate';
-
 import { Editable, Slate, withReact } from 'slate-react';
+import type { RenderElementProps, RenderLeafProps } from 'slate-react';
+import { withHistory } from 'slate-history';
 
 import '../styles/Editor.css';
 import Toolbar from './Toolbar';
@@ -14,13 +15,110 @@ const initialValue = [
 ];
 
 function Editor(): React.ReactElement {
-    const [editor] = useState(() => withReact(createEditor()));
+    const renderElement: (props: RenderElementProps) => JSX.Element =
+        useCallback((props) => <Element {...props} />, []);
+    const renderLeaf: (props: RenderLeafProps) => JSX.Element = useCallback(
+        (props) => <Leaf {...props} />,
+        []
+    );
+    const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
     return (
         <Slate editor={editor} value={initialValue}>
             <Toolbar />
-            <Editable className="Editor" spellCheck autoFocus />
+            <Editable
+                className="Editor"
+                renderElement={renderElement}
+                renderLeaf={renderLeaf}
+                spellCheck
+                autoFocus
+            />
         </Slate>
     );
 }
+
+const Element: (props: RenderElementProps) => JSX.Element = ({
+    attributes,
+    children,
+    element
+}: RenderElementProps) => {
+    const style = {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        textAlign: (element as any).align ? (element as any).align : 'left'
+    };
+    switch ((element as any).type) {
+        case 'block-quote':
+            return (
+                <blockquote style={style} {...attributes}>
+                    {children}
+                </blockquote>
+            );
+        case 'bulleted-list':
+            return (
+                <ul style={style} {...attributes}>
+                    {children}
+                </ul>
+            );
+        case 'heading-one':
+            return (
+                <h1 style={style} {...attributes}>
+                    {children}
+                </h1>
+            );
+        case 'heading-two':
+            return (
+                <h2 style={style} {...attributes}>
+                    {children}
+                </h2>
+            );
+        case 'list-item':
+            return (
+                <li style={style} {...attributes}>
+                    {children}
+                </li>
+            );
+        case 'numbered-list':
+            return (
+                <ol style={style} {...attributes}>
+                    {children}
+                </ol>
+            );
+        default:
+            return (
+                <p style={style} {...attributes}>
+                    {children}
+                </p>
+            );
+    }
+};
+
+const Leaf: (props: RenderLeafProps) => JSX.Element = ({
+    attributes,
+    children,
+    leaf
+}: RenderLeafProps) => {
+    const leafCopy = leaf as any;
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (leafCopy.bold) {
+        children = <strong>{children}</strong>;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (leafCopy.code) {
+        children = <code>{children}</code>;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (leafCopy.italic) {
+        children = <em>{children}</em>;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (leafCopy.underline) {
+        children = <u>{children}</u>;
+    }
+
+    return <span {...attributes}>{children}</span>;
+};
 
 export default Editor;
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useSlate } from 'slate-react';
+import { Editor, Transforms, Element as SlateElement } from 'slate';
+import type { BaseEditor } from 'slate';
 
 import '../styles/Toolbar.css';
 import { ReactComponent as NewDocumentIcon } from '../assets/newDocument.svg';
@@ -16,24 +19,210 @@ import { ReactComponent as JustifyAlignIcon } from '../assets/justifyAlign.svg';
 
 import ToolbarButton from './ToolbarButton';
 
+const LIST_TYPES = ['numbered-list', 'bulleted-list'];
+const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify'];
+
 function Toolbar(): React.ReactElement {
+    const editor = useSlate();
     return (
         <div className="Toolbar">
             <ToolbarButton Icon={NewDocumentIcon} text="New File" />
             <ToolbarButton Icon={OpenDocumentIcon} text="Open" />
             <ToolbarButton Icon={SaveIcon} text="Save" />
             <div className="Divider" />
-            <ToolbarButton Icon={BoldIcon} isSecondaryButton />
-            <ToolbarButton Icon={ItalicIcon} isSecondaryButton />
-            <ToolbarButton Icon={UnderlineIcon} isSecondaryButton />
-            <ToolbarButton Icon={LeftAlignIcon} isSecondaryButton />
-            <ToolbarButton Icon={CenterAlignIcon} isSecondaryButton />
-            <ToolbarButton Icon={RightAlignIcon} isSecondaryButton />
-            <ToolbarButton Icon={JustifyAlignIcon} isSecondaryButton />
-            <ToolbarButton Icon={NumberListIcon} isSecondaryButton />
-            <ToolbarButton Icon={BulletListIcon} isSecondaryButton />
+            <ToolbarButton
+                Icon={BoldIcon}
+                isSecondaryButton
+                isActive={isMarkActive(editor, 'bold')}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleMark(editor, 'bold');
+                }}
+            />
+            <ToolbarButton
+                Icon={ItalicIcon}
+                isSecondaryButton
+                isActive={isMarkActive(editor, 'italic')}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleMark(editor, 'italic');
+                }}
+            />
+            <ToolbarButton
+                Icon={UnderlineIcon}
+                isSecondaryButton
+                isActive={isMarkActive(editor, 'underline')}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleMark(editor, 'underline');
+                }}
+            />
+            <ToolbarButton
+                Icon={LeftAlignIcon}
+                isSecondaryButton
+                isActive={isBlockActive(
+                    editor,
+                    'left',
+                    TEXT_ALIGN_TYPES.includes('left') ? 'align' : 'type'
+                )}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleBlock(editor, 'left');
+                }}
+            />
+            <ToolbarButton
+                Icon={CenterAlignIcon}
+                isSecondaryButton
+                isActive={isBlockActive(
+                    editor,
+                    'center',
+                    TEXT_ALIGN_TYPES.includes('center') ? 'align' : 'type'
+                )}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleBlock(editor, 'center');
+                }}
+            />
+            <ToolbarButton
+                Icon={RightAlignIcon}
+                isSecondaryButton
+                isActive={isBlockActive(
+                    editor,
+                    'right',
+                    TEXT_ALIGN_TYPES.includes('right') ? 'align' : 'type'
+                )}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleBlock(editor, 'right');
+                }}
+            />
+            <ToolbarButton
+                Icon={JustifyAlignIcon}
+                isSecondaryButton
+                isActive={isBlockActive(
+                    editor,
+                    'justify',
+                    TEXT_ALIGN_TYPES.includes('justify') ? 'align' : 'type'
+                )}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleBlock(editor, 'justify');
+                }}
+            />
+            <ToolbarButton
+                Icon={NumberListIcon}
+                isSecondaryButton
+                isActive={isBlockActive(
+                    editor,
+                    'numbered-list',
+                    TEXT_ALIGN_TYPES.includes('numbered-list')
+                        ? 'align'
+                        : 'type'
+                )}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleBlock(editor, 'numbered-list');
+                }}
+            />
+            <ToolbarButton
+                Icon={BulletListIcon}
+                isSecondaryButton
+                isActive={isBlockActive(
+                    editor,
+                    'bulleted-list',
+                    TEXT_ALIGN_TYPES.includes('bulleted-list')
+                        ? 'align'
+                        : 'type'
+                )}
+                onMouseDown={(event) => {
+                    event.preventDefault();
+                    toggleBlock(editor, 'bulleted-list');
+                }}
+            />
         </div>
     );
 }
+
+const toggleBlock: (editor: BaseEditor, format: string) => void = (
+    editor,
+    format
+) => {
+    const isActive = isBlockActive(
+        editor,
+        format,
+        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+    );
+    const isList = LIST_TYPES.includes(format);
+
+    Transforms.unwrapNodes(editor, {
+        match: (n) =>
+            !Editor.isEditor(n) &&
+            SlateElement.isElement(n) &&
+            LIST_TYPES.includes((n as any).type) &&
+            !TEXT_ALIGN_TYPES.includes(format),
+        split: true
+    });
+    let newProperties: Partial<any>;
+    if (TEXT_ALIGN_TYPES.includes(format)) {
+        newProperties = {
+            align: isActive ? undefined : format
+        };
+    } else {
+        newProperties = {
+            type: isActive ? 'paragraph' : isList ? 'list-item' : format
+        };
+    }
+    Transforms.setNodes<SlateElement>(editor, newProperties);
+
+    if (!isActive && isList) {
+        const block = { type: format, children: [] };
+        Transforms.wrapNodes(editor, block);
+    }
+};
+
+const toggleMark: (editor: BaseEditor, format: string) => void = (
+    editor,
+    format
+) => {
+    const isActive = isMarkActive(editor, format);
+
+    if (isActive) {
+        Editor.removeMark(editor, format);
+    } else {
+        Editor.addMark(editor, format, true);
+    }
+};
+
+const isBlockActive: (
+    editor: BaseEditor,
+    format: string,
+    blockType: string
+) => boolean = (editor, format, blockType = 'type') => {
+    const { selection } = editor;
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!selection) return false;
+
+    const [match] = Array.from(
+        Editor.nodes(editor, {
+            at: Editor.unhangRange(editor, selection),
+            match: (n) =>
+                !Editor.isEditor(n) &&
+                SlateElement.isElement(n) &&
+                (n as any)[blockType] === format
+        })
+    );
+
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    return !!match;
+};
+
+const isMarkActive: (editor: Editor, format: string) => boolean = (
+    editor,
+    format
+) => {
+    const marks: any = Editor.marks(editor);
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    return marks ? marks[format] === true : false;
+};
 
 export default React.memo(Toolbar);
